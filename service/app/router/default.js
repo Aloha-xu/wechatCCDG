bbfb6870ae327a605eda5d9631697943c87f5fbc
Aloha-xu <598392491@qq.com>
2021-10-18 11:07:02 +0800

    1

--- service/app/router/default.js
+++ service/app/router/default.js
@@ -1,8 +1,7 @@
 module.exports = app => {
     const { router, controller } = app;
     router.get('/getArticleList', controller.home.getArticleList);
-    router.get('/getArticleById', controller.home.getArticleById);
+    router.post('/addLikeCount', controller.home.addLikeCount);
     router.post('/addArticle', controller.home.addArticle);
     router.post('/addComment', controller.home.addComment);
-    router.get('/getCommentById/:id', controller.home.getCommentById);
   };
--- view/pages/friend/friend.js
+++ view/pages/friend/friend.js
@@ -1,18 +1,8 @@
-// pages/friend/friend.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    isChecked: false,
     articleDetail: null,
     commentConent: '',
-  },
-  changeRadio() {
-    this.setData({
-      isChecked: !this.data.isChecked
-    })
+    isShowComment: null,
   },
   bindinputFun(e) {
     this.setData({
@@ -26,11 +16,21 @@ Page({
       method: "GET",
       url: "http://127.0.0.1:7001/getArticleList",
       success: function (res) {
+        let isShowComment = []
+        for (let i = 0; i < res.data.data.length; i++) {
+          isShowComment.push({
+            isShow: false
+          })
+        }
         that.setData({
-          articleDetail: res.data.data
+          articleDetail: res.data.data,
+          isShowComment: isShowComment
         })
       }
     })
+
+
+
   },
   submit(e) {
     let that = this
@@ -53,11 +53,49 @@ Page({
       commentConent: ""
     })
   },
+  handleLikeFun(e) {
+    let likeCount = this.data.articleDetail[e.currentTarget.dataset.index].likeCount
+    let id = this.data.articleDetail[e.currentTarget.dataset.index].id
+    let isLike = this.data.articleDetail[e.currentTarget.dataset.index].isLike
+    if (isLike) {
+      likeCount = likeCount + 1
+    } else {
+      likeCount = likeCount - 1
+    }
+    this.setData({
+      [`articleDetail[${e.currentTarget.dataset.index}].likeCount`]: likeCount,
+      [`articleDetail[${e.currentTarget.dataset.index}].isLike`]: !isLike
+    })
+    wx.request({
+      url: "http://127.0.0.1:7001/addLikeCount",
+      method: 'post',
+      data: {
+        likeCount: likeCount,
+        id: id
+      },
+      success: function (res) {
+
+      },
+    })
+
+  },
+  handleCommentShow(e) {
+    var isshow = null
+    if (this.data.isShowComment[e.currentTarget.dataset.index].isShow) {
+      isshow = false
+    } else {
+      isshow = true
+    }
+    this.setData({
+      [`isShowComment[${e.currentTarget.dataset.index}].isShow`]: isshow
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.getAstricleDetail()
+
   },
 
   /**
